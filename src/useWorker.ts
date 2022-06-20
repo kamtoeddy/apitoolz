66a8ad80c75e55f20d6717bfa39f3b6510e7e4b6
kamtoeddy <lollipopkalog@gmail.com>
2022-06-20 12:05:31 +0100

    started work on useWorker

---
+++ src/useWorker.ts
@@ -0,0 +1,32 @@
+import { Worker } from "worker_threads";
+
+import { ApiError } from "./ApiError";
+
+interface useWorkerProps {
+  data: any;
+  event?: string;
+  path: string;
+}
+
+export const useWorker = ({
+  path = "",
+  event = "handle-data",
+  data,
+}: useWorkerProps) => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(path, {
+      workerData: { event, data: JSON.stringify(data) },
+    });
+    worker.on("message", resolve);
+    worker.on("error", reject);
+    worker.on("exit", (code: number) => {
+      if (code !== 0) {
+        reject(
+          new ApiError({
+            message: `Worker stopped executing ${event} with code ${code}`,
+          })
+        );
+      }
+    });
+  });
+};
