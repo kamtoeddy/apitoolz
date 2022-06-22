99e37d7a2cb6ed13cc901a1a2e5134cf39f9d218
kamtoeddy <lollipopkalog@gmail.com>
2022-06-22 17:41:02 +0100

    added loadEnvVars helper

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./ApiError";
 export * from "./loadCircular";
+export * from "./loadEnvVars";
 export * from "./utils/isOfType";
 export * from "./sanitize";
 export * from "./useController";
---
+++ src/loadEnvVars.ts
@@ -0,0 +1,46 @@
+import { ApiError } from "./ApiError";
+import { looseObject } from "./interfaces";
+import { isOfType } from "./utils/isOfType";
+
+const getEnvVars = (vars: looseObject[] = [], defaults: looseObject = {}) => {
+  let _vars: looseObject = {};
+
+  vars.forEach(({ name, parser }) => {
+    let val = process.env?.[name] ?? defaults?.[name];
+
+    _vars[name] = parser ? parser(val) : val;
+  });
+
+  return _vars;
+};
+
+export const LoadEnvVars = (
+  vars: looseObject[],
+  defaults: looseObject = {}
+) => {
+  const payload: looseObject = {};
+
+  try {
+    vars.forEach(({ name, parser }, i) => {
+      payload[i] = [];
+      if (!name?.trim()) payload[i].push("Invalid name");
+
+      if (parser && !isOfType(parser, "function"))
+        payload[i].push("Invalid parser");
+
+      if (!payload[i].length) delete payload[i];
+    });
+
+    if (Object.keys(payload).length)
+      throw new ApiError({
+        message: "Invalid env vars",
+        payload,
+        statusCode: 500,
+      });
+  } catch (err) {
+    console.log(err);
+    return {};
+  }
+
+  return getEnvVars(vars, defaults);
+};
