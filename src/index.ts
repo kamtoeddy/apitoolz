72e14cec18581b6421beebb01a5e1df98dc93a60
kamtoeddy <lollipopkalog@gmail.com>
2022-06-22 17:05:18 +0100

    added helper isOfType

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./ApiError";
 export * from "./loadCircular";
+export * from "./utils/isOfType";
 export * from "./sanitize";
 export * from "./useController";
 export * from "./useWorker";
---
+++ src/utils/isOfType.ts
@@ -0,0 +1,1 @@
+export const isOfType = (v: any, _type: string = "") => typeof v == _type;
