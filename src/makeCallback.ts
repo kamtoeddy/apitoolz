1b33754424e5dbd9258f1e334e3616928877522c
kamtoeddy <lollipopkalog@gmail.com>
2022-06-25 10:10:25 +0100

    added makeCallback

---
+++ src/makeCallback.ts
@@ -0,0 +1,38 @@
+import { looseObject } from "./interfaces";
+
+type ControllerType = (req: looseObject) => Promise<any>;
+
+export const makeCallback = (controller: ControllerType) => {
+  return (req: looseObject, res: looseObject) => {
+    const Req = {
+      body: req.body,
+      ip: req.ip,
+      method: req.method,
+      path: req.path,
+      params: req.params,
+      query: req.query,
+      user: req.user,
+      headers: {
+        "Content-Type": req.get("Content-Type"),
+        referer: req.get("referer"),
+        "User-Agent": req.get("User-Agent"),
+      },
+    };
+
+    controller(Req)
+      .then((Res: looseObject) => {
+        if (Res.headers) res.set(Res.headers);
+
+        if (Res.cookies) {
+          Res.cookies.forEach((cookie: looseObject) => {
+            res.cookie(cookie.key, cookie.value, cookie.options);
+          });
+        }
+
+        res.status(Res?.statusCode ?? 200).json(Res.body);
+      })
+      .catch((err: Error) => {
+        res.status(500).send({ message: err.message, payload: {} });
+      });
+  };
+};
