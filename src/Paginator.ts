0b8881af2a45a2ac378b3c17d433d20f86f7cf8e
kamtoeddy <lollipopkalog@gmail.com>
2022-07-12 11:04:31 +0100

    added Paginator class for Api pagination

---
+++ src/Paginator.ts
@@ -0,0 +1,53 @@
+import { looseObject } from "./interfaces";
+
+interface PaginatorProps {
+  limit: number;
+  page: number;
+}
+
+interface PaginateProps {
+  count: number;
+  data: any[];
+}
+
+export class Paginator {
+  private limit: number = 0;
+  private maxLimit: number;
+  private page: number = 0;
+
+  constructor(maxLimit: number, { limit, page }: PaginatorProps) {
+    this.maxLimit = maxLimit;
+
+    this.limit = limit > this.maxLimit ? this.maxLimit : limit;
+    this.page = page;
+  }
+
+  getEndIndex = () => this.page * this.limit;
+
+  getStartIndex() {
+    if (!this.limit || !this.page) return 0;
+
+    return (this.page - 1) * this.limit;
+  }
+
+  paginate({ count, data }: PaginateProps) {
+    const paginated: looseObject = { data };
+
+    const startIndex = this.getStartIndex(),
+      stopIndex = this.getEndIndex();
+
+    if (startIndex > 0) {
+      paginated.previous = { limit: this.limit, page: this.page - 1 };
+    }
+
+    if (stopIndex < count) {
+      paginated.next = { limit: this.limit, page: this.page + 1 };
+    }
+
+    if (data.length > stopIndex - startIndex) {
+      paginated.data = data.slice(startIndex, stopIndex);
+    }
+
+    return paginated;
+  }
+}
