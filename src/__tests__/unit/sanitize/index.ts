2c943233ef98112f7900335ee3ee92790abbaf63
kamtoeddy <lollipopkalog@gmail.com>
2022-09-01 07:28:24 +0100

    did tests for sanitize.replace

--- src/__tests__/unit/sanitize/index.ts
+++ src/__tests__/unit/sanitize/index.ts
@@ -12,27 +12,27 @@ export const sanitize_Tests = ({ sanitize }: { sanitize: Function }) => {
   beforeEach(() => {
     users = [
       {
+        bio: { twitter: { displayName: "jamezz", link: "/facebook/jamezz" } },
+        dob: "02/02/1895",
+        joinDate: "today",
         name: "James",
         password: "1234",
-        joinDate: "today",
-        dob: "02/02/1895",
-        bio: { twitter: { displayName: "jamezz", link: "/facebook/jamezz" } },
       },
       {
-        name: "Mary",
-        password: "1234",
-        joinDate: "today",
-        dob: "02/07/1905",
         bio: {
           twitter: { displayName: "mary_jane", link: "/facebook/mary_jane" },
         },
+        dob: "02/07/1905",
+        joinDate: "today",
+        name: "Mary",
+        password: "1234",
       },
       {
+        bio: { twitter: { displayName: "bobby", link: "/facebook/bobby" } },
+        dob: "02/02/1900",
+        joinDate: "today",
         name: "Bob",
         password: "1234",
-        joinDate: "today",
-        dob: "02/02/1900",
-        bio: { twitter: { displayName: "bobby", link: "/facebook/bobby" } },
       },
     ];
   });
@@ -46,10 +46,10 @@ export const sanitize_Tests = ({ sanitize }: { sanitize: Function }) => {
 
     it("should remove specified fields", () => {
       const getUser = (user: User) => ({
-        name: user.name,
+        bio: user.bio,
         dob: user.dob,
         joinDate: user.joinDate,
-        bio: user.bio,
+        name: user.name,
       });
 
       const user = users[0];
@@ -68,9 +68,9 @@ export const sanitize_Tests = ({ sanitize }: { sanitize: Function }) => {
 
     it("should remove multiple fields", () => {
       const getUser = (user: User) => ({
-        name: user.name,
         dob: user.dob,
         joinDate: user.joinDate,
+        name: user.name,
       });
 
       const user = users[0];
@@ -87,10 +87,10 @@ export const sanitize_Tests = ({ sanitize }: { sanitize: Function }) => {
 
     it("should remove nested fields", () => {
       const getUser = (user: User) => ({
-        name: user.name,
+        bio: { twitter: { displayName: user.bio.twitter.displayName } },
         dob: user.dob,
         joinDate: user.joinDate,
-        bio: { twitter: { displayName: user.bio.twitter.displayName } },
+        name: user.name,
       });
 
       const user = users[0];
@@ -99,6 +99,98 @@ export const sanitize_Tests = ({ sanitize }: { sanitize: Function }) => {
       const sanitizedUsers = users.map((user) => getUser(user));
 
       const option = { remove: ["bio.twitter.link", "password"] };
+
+      expect(sanitize(user, option)).toEqual(sanitizedUser);
+
+      expect(sanitize(users, option)).toEqual(sanitizedUsers);
+    });
+
+    it("should replace specified fields", () => {
+      const getUser = (user: User) => ({
+        bio: user.bio,
+        dateJoined: user.joinDate,
+        dob: user.dob,
+        name: user.name,
+        password: user.password,
+      });
+
+      const user = users[0];
+      const sanitizedUser = getUser(user);
+
+      const sanitizedUsers = users.map((user) => getUser(user));
+
+      const option = { replace: { joinDate: "dateJoined" } };
+
+      expect(sanitize(user, option)).toEqual(sanitizedUser);
+
+      expect(sanitize(users, option)).toEqual(sanitizedUsers);
+    });
+
+    it("should replace multiple fields", () => {
+      const getUser = (user: User) => ({
+        bio: user.bio,
+        dateOfBirth: user.dob,
+        dateJoined: user.joinDate,
+        name: user.name,
+        password: user.password,
+      });
+
+      const user = users[0];
+      const sanitizedUser = getUser(user);
+
+      const sanitizedUsers = users.map((user) => getUser(user));
+
+      const option = {
+        replace: { joinDate: "dateJoined", dob: "dateOfBirth" },
+      };
+
+      expect(sanitize(user, option)).toEqual(sanitizedUser);
+
+      expect(sanitize(users, option)).toEqual(sanitizedUsers);
+    });
+
+    it("should replace nested fields", () => {
+      const getUser = (user: User) => ({
+        bio: { twitter: { displayName: user.bio.twitter.displayName } },
+        dateOfBirth: user.dob,
+        joinDate: user.joinDate,
+        name: user.name,
+        password: user.password,
+        twitterLink: user.bio.twitter.link,
+      });
+
+      const user = users[0];
+      const sanitizedUser = getUser(user);
+
+      const sanitizedUsers = users.map((user) => getUser(user));
+
+      const option = {
+        replace: { "bio.twitter.link": "twitterLink", dob: "dateOfBirth" },
+      };
+
+      expect(sanitize(user, option)).toEqual(sanitizedUser);
+
+      expect(sanitize(users, option)).toEqual(sanitizedUsers);
+    });
+
+    it("should remove & replace fields", () => {
+      const getUser = (user: User) => ({
+        bio: { twitter: { displayName: user.bio.twitter.displayName } },
+        dateOfBirth: user.dob,
+        joinDate: user.joinDate,
+        name: user.name,
+        twitterLink: user.bio.twitter.link,
+      });
+
+      const user = users[0];
+      const sanitizedUser = getUser(user);
+
+      const sanitizedUsers = users.map((user) => getUser(user));
+
+      const option = {
+        remove: "password",
+        replace: { "bio.twitter.link": "twitterLink", dob: "dateOfBirth" },
+      };
 
       expect(sanitize(user, option)).toEqual(sanitizedUser);
 
