0a26a8b91089b472813a3e6ad657d316363888dc
kamtoeddy <lollipopkalog@gmail.com>
2022-09-02 07:21:14 +0100

    added module name suggestions to loadCircular

--- src/loadCircular.ts
+++ src/loadCircular.ts
@@ -4,15 +4,19 @@ import { _getCallerFile } from "./utils/_getCallerFile.js";
 import { join } from "path";
 
 const loader =
-  (modules: ObjectType, dirName: string) =>
-  (name: string = "") => {
+  <T extends ObjectType>(modules: T, dirName: string) =>
+  (name: keyof T) => {
     const _module = modules?.[name];
 
-    if (!_module) throw new ApiError({ message: `${name} is not registered` });
+    if (!_module)
+      throw new ApiError({
+        message: "Unknown Circular module",
+        payload: { [name]: ["This module may is not registered"] },
+      });
 
     return require(join(dirName, _module));
   };
 
-export const registerModules = (modules: ObjectType) => {
+export function registerModules<T extends ObjectType>(modules: T) {
   return { loadCircular: loader(modules, _getCallerFile(2, true)) };
-};
+}
