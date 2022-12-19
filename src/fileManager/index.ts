7e26538813326b4024e412908adc9e6cdb1b77a7
kamtoeddy <lollipopkalog@gmail.com>
2022-12-19 08:36:02 +0100

    did clean-ups in fileManager

--- src/fileManager/index.ts
+++ src/fileManager/index.ts
@@ -1,23 +1,35 @@
 import fs from "fs";
-import {dirname} from "path";
+import { dirname } from "path";
 import { toArray } from "../utils/toArray";
 
 export { parser as parseMultipartData } from "./parseMultipartData";
+
+export {
+  copyFile,
+  deleteFile,
+  deleteFilesAt,
+  deleteFolder,
+  getFileExtention,
+  getFileName,
+  getFilePath,
+  getFileType,
+};
 
 const isExistingPath = (path: string) => {
   return fs.existsSync(dirname(path));
 };
 
-export function copyFile(from: string, to: string) {
+function copyFile(from: string, to: string) {
   if (!isExistingPath(from)) return;
 
-  const directoryTo = dirname(to)
-  if (!isExistingPath(directoryTo)) fs.mkdirSync(directoryTo, { recursive: true });
+  const directoryTo = dirname(to);
+  if (!isExistingPath(directoryTo))
+    fs.mkdirSync(directoryTo, { recursive: true });
 
   fs.copyFileSync(from, to, fs.constants.COPYFILE_FICLONE);
 }
 
-export function cutFile(from: string, to: string) {
+function cutFile(from: string, to: string) {
   try {
     copyFile(from, to);
     deleteFile(from);
@@ -26,35 +38,31 @@ export function cutFile(from: string, to: string) {
   }
 }
 
-export function deleteFile(path: string) {
+function deleteFile(path: string) {
   if (isExistingPath(path)) fs.unlinkSync(path);
 }
 
-export function deleteFilesAt(paths: string | string[] = []) {
+function deleteFilesAt(paths: string | string[] = []) {
   paths = toArray(paths);
   for (let path of paths) deleteFile(path);
 }
 
-export async function deleteFolder(path: string) {
+async function deleteFolder(path: string) {
   if (!isExistingPath(path)) return;
   return fs.rm(path, { recursive: true }, (err) => {
     if (err) console.log(`error deleting folder @${path}:`, err);
   });
 }
 
-function getDirectoryName(path:string){
-
-}
-
-export function getFileExtention(mimetype: string | null) {
+function getFileExtention(mimetype: string | null) {
   return String(mimetype).split("/")?.[1];
 }
 
-export function getFileName(path: string) {
+function getFileName(path: string) {
   return path.substring(path.lastIndexOf("/") + 1);
 }
 
-export const getFilePath = (uploadDir: string) => (path: string) => {
+const getFilePath = (uploadDir: string) => (path: string) => {
   const uploadDirIndex = path.indexOf(uploadDir);
 
   return uploadDirIndex != -1
@@ -62,6 +70,6 @@ export const getFilePath = (uploadDir: string) => (path: string) => {
     : path;
 };
 
-export function getFileType(mimetype: string) {
+function getFileType(mimetype: string) {
   return mimetype.split("/")[0];
 }
