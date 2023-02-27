e2fa7194d916a665066262ab571413198d71f763
kamtoeddy <kamtoeddytagne@gmail.com>
2023-02-27 18:42:26 +0100

    feat: made Paginator class production ready

--- src/Paginator.ts
+++ src/Paginator.ts
@@ -1,50 +1,104 @@
-import { ObjectType } from "./types";
-
-export interface PaginatorProps {
-  limit: number;
+export type PaginatedOptions<T> = {
+  data: T[];
   page: number;
-}
+};
 
-export interface PaginateProps {
-  count: number;
-  data: any[];
-}
+export type PaginateOptions<T> = {
+  data: T[];
+  page: number;
+  totalRecords: number;
+};
+
+export type PaginatedData<T> = {
+  currentPage: number;
+  data: T[];
+  limit: number;
+  nextPage: null | number;
+  previousPage: null | number;
+  totalPages: number;
+  totalRecords: number;
+};
 
 export class Paginator {
-  private _limit: number = 0;
   private _maxLimit: number;
-  private _page: number = 0;
+  private _limit: number;
 
-  constructor(maxLimit: number, { limit, page }: PaginatorProps) {
+  constructor(maxLimit: number, limit = 10) {
     this._maxLimit = maxLimit;
-
-    this._limit = limit > this._maxLimit ? this._maxLimit : limit;
-    this._page = page;
+    this._limit = limit;
   }
 
-  getEndIndex = () => this._page * this._limit;
-
-  getStartIndex() {
-    if (!this._limit || !this._page) return 0;
-
-    return (this._page - 1) * this._limit;
+  get limit() {
+    return this._limit;
   }
 
-  paginate({ count, data }: PaginateProps) {
-    const paginated: ObjectType = { data };
+  private _getPaginatedData<T>({
+    data,
+    page,
+    totalRecords,
+  }: PaginateOptions<T>): PaginatedData<T> {
+    if (page < 1) page = 1;
 
-    const startIndex = this.getStartIndex(),
-      stopIndex = this.getEndIndex();
+    const paginated: PaginatedData<T> = {
+      currentPage: page,
+      data: [],
+      limit: this._limit,
+      nextPage: null,
+      previousPage: null,
+      totalPages: Math.ceil(totalRecords / this.limit),
+      totalRecords,
+    };
 
-    if (startIndex > 0)
-      paginated.previous = { limit: this._limit, page: this._page - 1 };
+    const startIndex = this.getOffset(page),
+      stopIndex = this.getEndIndex(page);
 
-    if (stopIndex < count)
-      paginated.next = { limit: this._limit, page: this._page + 1 };
+    if (startIndex > totalRecords) {
+      const previousPageNumber = page - 1;
+      const prevStartIndex = this.getOffset(previousPageNumber);
+
+      if (prevStartIndex <= totalRecords)
+        paginated.previousPage = previousPageNumber;
+
+      return paginated;
+    }
+
+    paginated.data = data;
+
+    if (startIndex > 0) paginated.previousPage = page - 1;
+
+    if (stopIndex < totalRecords) paginated.nextPage = page + 1;
 
     if (data.length > stopIndex - startIndex)
       paginated.data = data.slice(startIndex, stopIndex);
 
     return paginated;
+  }
+
+  getEndIndex = (page: number) => page * this._limit;
+
+  /**
+   * To the index of the first item of a page in the entire array. Look at it like the number of items skipped to get to the first item on the specified page
+   *
+   * Could also be called getStartIndex
+   * @param page the current page
+   */
+  getOffset(page: number) {
+    return !this._limit || !page ? 0 : (page - 1) * this._limit;
+  }
+
+  seLimit(limit: number) {
+    if (limit <= this._maxLimit) this._limit = limit;
+
+    return this;
+  }
+
+  getPaginatedData<T>({ data, page }: PaginatedOptions<T>) {
+    return this._getPaginatedData({ data, totalRecords: data.length, page });
+  }
+
+  paginate<T>({ data, totalRecords, page }: PaginateOptions<T>) {
+    if (!totalRecords || totalRecords < data.length) totalRecords = data.length;
+
+    return this._getPaginatedData({ data, page, totalRecords });
   }
 }
